91c13ff33425b5f39c26df2a4194e0985c876d0a
Anetor <anetor.ataga@gmail.com>
2023-09-01 01:38:24 +0100

    Phonebook with c#. Using the different foundational concepts  to build a phone book that displays on the console.

---
+++ PhoneBook.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class PhoneBook
+    {
+        private List<Contact> _contacts {  get; set; } = new List<Contact>();
+
+        private void DisplayOnContactDetails(Contact contact)
+        {
+            Console.WriteLine("Contact name: {0}, Contact number: {1}", contact.Name, contact.Number);
+        }
+        private void DisplayLoop(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayOnContactDetails(contact);
+            }
+
+        }
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+
+            if(contact == null)
+            {
+                Console.WriteLine("Contact not available");
+            } else
+            {
+                DisplayOnContactDetails(contact);
+           }
+        }
+
+        public void GetAllContact()
+        {
+            DisplayLoop(_contacts);
+        } 
+
+        public void DisplaySearchContact(string searchString)
+        {
+            //searchString = Console.ReadLine();
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchString)).ToList();
+
+
+            DisplayLoop(matchingContacts);
+
+            
+           
+        }
+    }
+}   
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace PhoneBook
 {
@@ -6,7 +7,65 @@ namespace PhoneBook
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Welcome to the PhoneBook app!");
+            Console.WriteLine("Select operation");
+            Console.WriteLine("1 - Add contact");
+            Console.WriteLine("2 - Display contact by number");
+            Console.WriteLine("3 - View all contacts");
+            Console.WriteLine("4 - Search for specific contact");
+            Console.WriteLine("x - End operation");
+
+
+            var userInput = Console.ReadLine();
+
+            PhoneBook phonebook = new PhoneBook();
+
+            while(true)
+            {
+                switch (userInput.ToLower())
+                {
+                    case "1":
+
+                        Console.WriteLine("Enter contact name: ");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("Enter contact Number: ");
+                        var number = Console.ReadLine();
+
+                        var newContact = new Contact(name, number);
+                        phonebook.AddContact(newContact);
+
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter contact number to search: ");
+                        var contactNumber = Console.ReadLine();
+                        phonebook.DisplayContact(contactNumber);
+                        break;
+
+                    case "3":
+                        phonebook.GetAllContact();
+                        break;
+                    case "4":
+                        Console.WriteLine("Enter search term: ");
+                        var searchString = Console.ReadLine();
+
+                        phonebook.DisplaySearchContact(searchString);
+                        break;
+                    case "x":
+                        return;
+                        
+                    default:
+                        Console.WriteLine("Select a valid operation");
+                        break;
+                }
+
+                Console.WriteLine("Select another operation");
+                userInput = Console.ReadLine();
+
+            }
+
+           
         }
+
     }
 }
